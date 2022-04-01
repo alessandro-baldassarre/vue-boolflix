5fcf747f038e0b19482b2db92ab94363b16fb7d9
alessandro-baldassarre <alessandro.balda@hotmail.it>
2022-04-01 15:50:16 +0200

    added country flag

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -9,6 +9,7 @@
         </li>
         <li>
           {{findedElement.original_language}}
+          {{flag()}}
         </li>
         <li>
           {{findedElement.popularity}}
@@ -18,11 +19,29 @@
 </template>
 
 <script>
+// import { hasFlag } from 'country-flag-icons'
+import getUnicodeFlagIcon from 'country-flag-icons/unicode'
+
 export default {
   name: "MainHeader",
   props: {
     findedElement : Object,
   },
+  data: function () {
+    return {
+      flagToSearch: "",
+    };
+  },
+  methods:{
+    flag(){
+      this.flagToSearch = this.findedElement.original_language;
+      if(this.flagToSearch === 'en'){
+        this.flagToSearch = 'us';
+      }
+
+      return getUnicodeFlagIcon(this.flagToSearch.toUpperCase());
+    }
+  }
 };
 </script>
 
