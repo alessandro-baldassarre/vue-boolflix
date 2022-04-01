51da198d6c39f3a627fba5bc0543c5bcedc200b7
alessandro-baldassarre <alessandro.balda@hotmail.it>
2022-04-01 12:11:45 +0200

    started project with basic structure

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header>
+    <h1 class="text-danger">title</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "MainHeader",
+  props: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "@/assets/scss/partials/_variables";
+</style>
