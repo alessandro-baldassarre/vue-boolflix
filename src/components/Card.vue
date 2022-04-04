9a21ee0aa7f2afa288f33cc5f6ac58aa04ad7fd5
alessandro-baldassarre <alessandro.balda@hotmail.it>
2022-04-04 16:07:58 +0200

    update style single card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,29 +1,51 @@
 <template>
-  <div class="card-wrapper shadow" @mouseover="hover = true" @mouseleave="hover = false" >
-    <div v-if="(hover)" class="card-info text-white p-1">
-       <ul>
-      <li>
-        Titolo: {{ title() }}
-      </li>
-      <li v-if="(findedElement.original_title != findedElement.title) || (findedElement.name != findedElement.original_name)">
-        Titolo originale: {{ originalTitle() }}
-      </li>
-      <li>
-       Lingua: {{ flag() }}
-      </li>
-      <li>
-       Voto: {{ finalVote() }}
-        <span v-for="index in finalVote()" :key="index" class="text-warning">
-          <font-awesome-icon icon="fa-solid fa-star" />
-        </span>
-      </li>
+  <div
+    class="card-wrapper"
+    @mouseover="hover = true"
+    @mouseleave="hover = false"
+  >
+    <div v-if="hover" class="card-info text-white p-1">
+      <img
+        class="card-info-img"
+        :src="posterUrl()"
+        :alt="'poster of ' + title()"
+      />
+      <div class="card-info-text pt-3 ps-1 d-flex flex-column justify-content-evenly">
+        <div>Titolo: {{ title() }}</div>
+        <div
+          v-if="
+            findedElement.original_title != findedElement.title ||
+            findedElement.name != findedElement.original_name
+          "
+        >
+          Titolo originale: {{ originalTitle() }}
+        </div>
+        <div>Lingua: {{ flag() }}</div>
+        <div class="d-flex justify-content-between">
+          <div class="vote me-4">
+            Voto:
+          <span v-for="index in finalVote()" :key="index" class="text-warning">
+            <font-awesome-icon icon="fa-solid fa-star" />
+          </span>
+          </div>
+          <div class="icon-find-more pe-3">
+            <span class="rounded-circle border border-light p-1">
+              <font-awesome-icon icon="fa-solid fa-chevron-down" />
+            </span>
+          </div>
+        </div>
+      </div>
+
       <!-- <li class="overview">
        Overview: {{ findedElement.overview}}
       </li> -->
-    </ul>
     </div>
     <div v-else class="card-poster">
-      <img :src="posterUrl()" :alt="'poster of ' + title()"/>
+      <img
+        class="card-poster-img"
+        :src="posterUrl()"
+        :alt="'poster of ' + title()"
+      />
     </div>
   </div>
 </template>
@@ -89,7 +111,6 @@ export default {
 
 .card-wrapper {
   height: 100%;
-  cursor: pointer;
   .card-poster {
     height: 100%;
     img {
@@ -97,19 +118,41 @@ export default {
       height: 100%;
       border-radius: 15px;
       object-fit: cover;
+      object-position: center;
     }
   }
-  .card-info{
-    background-color: $mainBgDark;
-    border: 1px solid black;
-    border-radius: 15px;
-    // transform: scale(1.2);
+  .card-info {
+    background-color: #181818;
     height: 100%;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    border-radius: 15px;
+    transform: scale(1.6, 1.24);
+    img {
+      width: 100%;
+      height: 50%;
+      border-radius: 15px;
+      object-fit: cover;
+      object-position: top;
+    }
+    .card-info-text{
+      height: 50%;
+      font-size: .8rem;
+      @media screen and (max-width: 1250px) {
+        font-size: .65rem;
+      }
+    }
+    .icon-find-more{
+      span{
+        cursor: pointer;
+        &:hover{
+          background-color: $mainBgDark;
+        }
+      }
+    }
   }
-  .overview{
+  .overview {
     max-height: 30%;
-    overflow-y:auto;
+    overflow-y: auto;
   }
-  
 }
 </style>
